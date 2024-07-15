cd4b3090fe66643aee12e22350871ecc1ecaeede
nsjadon29 <nsjadon29@gmail.com>
2024-07-15 17:04:47 +0530

    started to build

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+      <Navbar />
+      <News />
+      </>
+    )
+  }
+}
